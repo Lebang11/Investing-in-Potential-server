ce64747599c2844ab8e295e939a01d0e3a7917d6
Lebang Nong <lebangnong@gmail.com>
2024-12-12 22:06:49 +0200

    Added more cors configuration

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://www.investinginpotential.co.za', // Replace with your frontend's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true, // Include credentials if needed
+};
 
+app.use(cors(corsOptions));
 
 // we need to import the user schema
 const User = require('./database/Schema/User')
