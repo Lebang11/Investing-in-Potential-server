f281935961774d09407862f1abde6f8ac3e699c6
Lebang Nong <lebangnong@gmail.com>
2024-12-27 12:19:00 +0200

    removed userID from payment

--- database/Schema/Payment.js
+++ database/Schema/Payment.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
 const PaymentSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
     email: {
         type: String,
         required: true
